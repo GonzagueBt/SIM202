7db597e1757fba7b0684697daf5a97ed7e2cf9ac
Gonzague <gonzague.bt@gmail.com>
2022-02-01 11:01:37 +0100

    modif Obstacle

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -1,14 +1,12 @@
 #include "Obstacle.hpp"
 
 
-//////// Intersection entre 2 segments AB et CD /////////
-// https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
-double ccw(Point A, Point B, Point C){
-    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x);
+Obstacle::Obstacle(Point *x = nullptr, double n=0){
+    nbr_sommets = n;
+    Sommets = x;
+    segValides = constructionSeg(nbr_sommets, Sommets);
 }
 
-double intersect(Point A, Point B, Point C, Point D){
-    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D);
+list<Segment> constructionSeg(int nb, Point * pts){
+    Point premier = pts[0];
 }
-
-////////////////////////////////////////////////////////////
--- Obstacle.hpp
+++ Obstacle.hpp
@@ -1,6 +1,7 @@
 #include "Point.hpp"
 #include "Arc.hpp"
 #include <iostream> 
+#include <list>
 using namespace std;
 #ifndef Project_H
 #define Project_H
@@ -11,9 +12,12 @@ class Obstacle
 public:
     int nbr_sommets;                            
     Point *Sommets; 
+    list<Segment> segValides;
     Obstacle(Point *x = nullptr, double n=0): Sommets(x), nbr_sommets(n){} // Constructeur par défaut                       
     ~Obstacle(); // Destructeur                     
     Obstacle(const Obstacle &Obs); // Constructeur par copie           
     bool &concave_convexe(const Obstacle &Obs);  
            
 };
+
+#endif Project_H
