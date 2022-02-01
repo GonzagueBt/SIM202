f92128bacc707e7d4c72922d12ab7050496da5f1
BalKanZ <98384730+BalKanZ@users.noreply.github.com>
2022-02-01 12:07:26 +0100

    Update Obstacle.hpp

--- Obstacle.hpp
+++ Obstacle.hpp
@@ -7,17 +7,29 @@ using namespace std;
 #define Project_H
 
 
+
 class Obstacle
 {
 public:
     int nbr_sommets;                            
     Point *Sommets; 
     list<Segment> segValides;
-    Obstacle(Point *x = nullptr, double n=0): Sommets(x), nbr_sommets(n){} // Constructeur par défaut                       
+    Obstacle(Point *x = nullptr, int n=0): Sommets(x), nbr_sommets(n){} // Constructeur par défaut                       
     ~Obstacle(); // Destructeur                     
-    Obstacle(const Obstacle &Obs); // Constructeur par copie           
-    bool &concave_convexe(const Obstacle &Obs);  
+    Obstacle(const Obstacle &Obs); // Constructeur par copie  
+    Point operator[](int i) const;           
+    bool concave_convexe(const Obstacle &Obs);  
+    bool inte_exte(const Point & P, const Obstacle &Obs); 
            
 };
 
-#endif Project_H
+
+// Fonction qui étant donné les coordonnées de deux points renvoie les coordonnées du vecteur normale
+Point Calcul_coor_Normale (const Point & S_1, const Point & S_2);
+// Produit scalaire entre deux vecteurs (un vecteur est tjrs représenté par un point)
+double Produit_Scalaire(const Point & a, const Point & b);
+
+
+
+
+#endif
