067e0e0ca0bc11c0cbd1f41fefbaf3034cc221e7
BalKanZ <98384730+BalKanZ@users.noreply.github.com>
2022-02-01 12:08:10 +0100

    Update Obstacle.cpp

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -1,7 +1,18 @@
+#include "Point.hpp"
+#include "Arc.hpp"
 #include "Obstacle.hpp"
+#include <list>
+extern ostream out;
 
 
-Obstacle::Obstacle(Point *x = nullptr, double n=0){
+Point Obstacle::operator[](int i) const
+{
+  return Sommets[i];
+}
+
+
+
+Obstacle::Obstacle(Point *x, int n){
     nbr_sommets = n;
     Sommets = x;
     segValides = constructionSeg(nbr_sommets, Sommets);
@@ -10,3 +21,60 @@ Obstacle::Obstacle(Point *x = nullptr, double n=0){
 list<Segment> constructionSeg(int nb, Point * pts){
     Point premier = pts[0];
 }
+
+
+
+Point Calcul_coor_Normale(const Point & S_1, const Point & S_2)
+{
+    Point S_milieu;
+    S_milieu = (S_1 + S_2) / 2;
+    Point P;
+    P = S_1 | S_2;
+    Point res;
+    res.x = - P.y;
+    res.y = P.x;
+    return res;
+}
+
+
+double Produit_Scalaire(const Point & a, const Point & b)
+{
+    double res;
+    res = (a.x * b.x) + (a.y * b.y);
+    return res;
+}
+
+double transfo(const Point & a, const Point & b, const Point & c)
+{
+    Point P;
+    P = Calcul_coor_Normale(a,b);
+    Point Q;
+    Q = Calcul_coor_Normale(b,c);
+    return Produit_Scalaire(P,Q);
+}
+
+
+bool Obstacle::concave_convexe(const Obstacle &Obs)
+{
+    int k = 0;
+    for (int i = 0; i < (Obs.nbr_sommets - 2); i++)
+    {
+        if (transfo(Obs[i], Obs[i+1], Obs[i+2]) < 0)
+            k += 1;
+    }
+    if (transfo(Obs[Obs.nbr_sommets - 2], Obs[Obs.nbr_sommets - 1], Obs[0]) < 0)
+        k+=1;
+    if (transfo(Obs[Obs.nbr_sommets - 1], Obs[0], Obs[1]) < 0)
+        k+=1;
+    if (k == nbr_sommets)
+        return true;
+    else if (k != nbr_sommets)
+        return false;
+}
+
+
+bool inte_exte(const Point & P, const Obstacle &Obs)
+{
+    
+}
+
