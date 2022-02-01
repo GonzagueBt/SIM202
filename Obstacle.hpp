ef5e4ef16239f94bfc48e054400841bf36d91ffb
BalKanZ <98384730+BalKanZ@users.noreply.github.com>
2022-02-01 10:52:29 +0100

    Update Obstacle.hpp

--- Obstacle.hpp
+++ Obstacle.hpp
@@ -1,9 +1,19 @@
+#include "Point.hpp"
 #include "Arc.hpp"
+#include <iostream> 
+using namespace std;
+#ifndef Project_H
+#define Project_H
 
-class Obstacle{
-    public :
-        Point * sommets;
-        Arc * normales;
-        
 
+class Obstacle
+{
+public:
+    int nbr_sommets;                            
+    Point *Sommets; 
+    Obstacle(Point *x = nullptr, double n=0): Sommets(x), nbr_sommets(n){} // Constructeur par défaut                       
+    ~Obstacle(); // Destructeur                     
+    Obstacle(const Obstacle &Obs); // Constructeur par copie           
+    bool &concave_convexe(const Obstacle &Obs);  
+           
 };
