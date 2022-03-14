957f07d7b7923442df8f3b62aa035ec70b72213b
maxence-jo <81634603+maxence-jo@users.noreply.github.com>
2022-03-14 17:52:18 +0100

    Add files via upload

---
+++ parserObstacle.h
@@ -0,0 +1,13 @@
+#ifndef PARSEROBSTACLE_H_INCLUDED
+#define PARSEROBSTACLE_H_INCLUDED
+
+#include <list>
+#include "Obstacle.h"
+
+/**
+ * @brief Création d'un fichier .txt contenant les informations des différents obstacles avant chevauchement
+ * @param l_obs : la liste d'obstacles à afficher
+ */
+void parserObstacle(list<Obstacle> l_obs);
+
+#endif // PARSEROBSTACLE_H_INCLUDED
