b6ac38b7935e57bafd0101fcd3e13a2a96a36603
BalKanZ <98384730+BalKanZ@users.noreply.github.com>
2022-03-14 16:30:14 +0100

    Update Obstacle.cpp

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -2,11 +2,55 @@
 #include "Arc.hpp"
 #include "Obstacle.hpp"
 
+// Avec la classe obstacle on cherche à retourner tous les segments valides d'un obstacle.
+// Un segment est dit valide si il joint deux sommets d'un obstacle sans passer par l'intérieur de cet obstacle.
+// Rmq: On a supposé dans toute cette section (et dans tout le projet) que les sommets d'un obstacle seront construits
+// dans le sens trigonométrique.
+
+
+
+// On regarde d'abord si l'obstacle est convexe ou concave.
+// Ce sera très utile pour construire la liste des segmenst valides
+
+
+//////////////////////////////////////////|-----------------------------------|//////////////////////////////////
+//////////////////////////////////////////| Concavité/Convexité de l'obstacle |//////////////////////////////////
+//////////////////////////////////////////|-----------------------------------|//////////////////////////////////
+
+
+Point Coor_Vecteur_Normale(const Point & S_1, const Point & S_2){
+    Point P = S_1 | S_2; // | oprérateur surchargé dans point.cpp
+    Point dir(P.y, -P.x);
+    // On utilise la matrice de rotation dans le sens horaire avec theta = Pi/2
+    return dir; // dir contient les coordonnées (sous forme de point) du vecteur normale sortant au segment [S_1,S_2]
+}
+
+vector<Point> Coor_Sommets_Normale(const Point & S_1, const Point & S_2){
+    vector<Point> res; // vecteur de points qui contient nos deux coordonnées: 
+    // point de départ du vecteur normale et point d'arrivée
+    Point debut = (S_1 + S_2) / 2; // le milieu du segment [S_1,S_2] est le point de départ du vecteur normale
+    res.push_back(debut);
+    Point direction = Coor_Vecteur_Normale(S_1, S_2); // la direction est donnée par la matrice de rotation
+    Point fin(debut.x + direction.x, debut.y + direction.y);
+    res.push_back(fin);
+    return res; // res contient deux points: le point de départ et celui d'arivée de la normale au segment [S_1,S_2]
+}
+
+// Avec transfo on regarde si l'angle formé par deux segments est concave ou convexe
+bool transfo(const Point & a, const Point & b, const Point & c){
+    vector<Point> P = Coor_Sommets_Normale(a,b);
+    vector<Point> Q = Coor_Sommets_Normale(b,c);
+    Point E1 = P[0] | P[1];
+    Point E2 = Q[0] | Q[1];
+    double angle = E1.x * E2.y - E2.x * E1.y;
+    if (angle > 0) return false; // transfo false => segment ab et bc convexe
+    return true; // transfo false => segment ab et bc concave
+}
 
 bool Obstacle::concave_convexe(){
-    if(this->nbr_sommets==1) return true;
-    // si l'angle algébrique entre deux normales est < 0, l'angle est obtu donc les deux normales ne s'intersectent pas
-    // il faut que tout couple de normales consécutives de notre obstacle vérifie cela pour que l'obstacle soit convexe
+    if(this->nbr_sommets==1) return true; // un point seul est considérée comme convexe
+    // l'obstacle est convexe ssi chacune des paires de ses segments forment un angle convexe
+    // la paire ext convexe pour transfo == false
     int k = 0; // compteur pour pouvoir conclure à la fin des boucles
     for (int i = 0; i < this->nbr_sommets - 2; i++) {
         if (transfo(this->Sommets[i], this->Sommets[i+1], this->Sommets[i+2]) == false) k += 1;
@@ -17,8 +61,73 @@ bool Obstacle::concave_convexe(){
     return false;
 }
 
+
+//////////////////////////////|-------------------------------------------------|////////////////////////////////
+//////////////////////////////| Construction des segments valides de l'obstacle |////////////////////////////////
+//////////////////////////////|-------------------------------------------------|////////////////////////////////
+
+
+// Un premier outil utile est de savoir si il y a une intersection entre deux segments
+
+//////////////////////////////////////////| Intersection de 2 Segments |///////////////////////////////////////////
+
+bool ccw(const Point &A, const Point &B, const Point &C){ 
+    return ((C.y-A.y) * (B.x-A.x)) > ((B.y-A.y) * (C.x-A.x));
+}
+
+bool intersect(const Point &A, const Point &B, const Point &C, const Point &D){
+    if(A==C || A==D || B==C || B==D) return false; // si il y a intersection sur les points de départs ou 
+                                                   // d'arrivées des segments on considère qu'il n'y a pas 
+                                                   // d'intersection
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D); // true si AB/BA intersecte CD/DC
+}
+
+bool intersect(const Segment A, const Segment B){return intersect(A.a, A.b, B.a, B.b);} // true si A intersecte B
+
+
+// Un second outil utile est de savoir si un point est à l'extérieur d'un obstacle
+
+//////////////////////////////////////////| Point extérieur à l'obstacle |///////////////////////////////////////////
+
+// Le principe de cette fonction: on prend notre point x, on trace un demi-droite vertical vers l'infini.
+// Si cette demi droite à un nombre d'intersection paire avec les contours de notre obstacle, alors x
+// est forcément à l'extérieur de l'obstacle.
+bool isOutside(Point x, Obstacle ob){
+    Point y(x.x, INT_MAX);
+    int cpt = 0;
+    list<Segment>::iterator its = ob.segValides_contour.begin();
+    for(; its!= ob.segValides_contour.end(); its++){
+        if(intersect(its->a, its->b, x, y)){
+            cpt++;
+        }
+    }
+    if(cpt%2 == 0) return true;
+    else{
+        //on refait une deuxieme fois le test avec une demi-droite horizontal pour gérer d'éventuel erreur quand x 
+        // se trouve sur un coté de l'obstacle
+        Point y(INT_MAX, x.y);
+        cpt = 0;
+        auto its = ob.segValides_contour.begin();
+        for(; its!= ob.segValides_contour.end(); its++){
+            if(intersect(its->a, its->b, x, y)){
+                cpt++;
+            }
+        }
+        if(cpt%2 == 0) return true;
+    }
+   return false;
+}
+
+////////////////////////////////// Fonction principale d'obstacle //////////////////////////////////////////
+// Cette fonction remplit deux listes: 
+//      - segValides_contour qui contient les segments qui sont sur les bords de l'obstacle
+//      - segValides_reste qui contient les segments qui joignent deux sommets de l'obstacle 
+//        et qui ne forment pas un segment sur le bord de l'obstacle
+
 void Obstacle::constructionSeg(){
-    if (concave_convexe() == true){ // on remplit la liste des segments valides en faisant le tour de l'obstacle
+    if (concave_convexe() == true){ 
+    // on remplit la liste des segments valides en faisant le tour de l'obstacle
+    // si l'obsatcle est convexe, ces segmenst seront tous ceux sur le bord
         for(int i = 0; i < this->nbr_sommets -1; i++){
             Segment var1(this->Sommets[i], this->Sommets[i+1]); 
             segValides_contour.push_back(var1);
@@ -26,13 +135,18 @@ void Obstacle::constructionSeg(){
         Segment var2(this->Sommets[this->nbr_sommets-1], this->Sommets[0]);
         segValides_contour.push_back(var2);
     }
-    else{ // on remplit la liste des segments valides en faisant le tour de l'obstacle et en considérant d'autres segmets valides dus à la concavité
+    else{ 
+    // on remplit la liste des segments valides en faisant le tour de l'obstacle et en considérant
+    // d'autres segmets valides dus à la concavité
         for(int i = 0; i < this->nbr_sommets - 1; i++){
             Segment var1(this->Sommets[i], this->Sommets[i+1]);
             segValides_contour.push_back(var1);
         }
         Segment var2(this->Sommets[this->nbr_sommets-1], this->Sommets[0]);
         segValides_contour.push_back(var2);
+        // Les conditions pour qu'un segment qui n'est pas sur le bord soit valide sont les suivantes:
+        // il ne doit pas intersecter un segment sur le bord de l'obstacle
+        // son milieu doit être à l'extérieur de l'obstacle
         for(int i = 0; i < this->nbr_sommets-1; i++){
             for(int j = i + 1; j < this->nbr_sommets; j++){
                 Segment arete(this->Sommets[i],this->Sommets[j]);
@@ -55,8 +169,12 @@ void Obstacle::constructionSeg(){
     }
 }
 
+
+// On termine cette classe avec deux fonctions "pratiques"
+
+// La première efface un segment d'une liste de segment
 void Obstacle::deleteSegFromList(Segment seg){
-    auto it = segValides_contour.begin();
+    list<Segment>::iterator it = segValides_contour.begin();
     for(; it!= segValides_contour.end(); it++){
         if(*it==seg){
             segValides_contour.erase(it);
@@ -65,86 +183,23 @@ void Obstacle::deleteSegFromList(Segment seg){
     }
 }
 
+// La seconde nous sert pour instancier les obstacles dans le main
 Obstacle ConstructObstacle(vector<Point> V){
     Obstacle Obs(V, V.size());
     Obs.constructionSeg();
     return Obs;
 }
 
-Point Coor_Vecteur_Normale(const Point & S_1, const Point & S_2){
-    Point P = S_1 | S_2;
-    Point dir(P.y, -P.x);
-    // On utilise la matrice de rotation dans le sens horaire avec theta = Pi/2
-    return dir;
-}
-
-
-vector<Point> Coor_Sommets_Normale(const Point & S_1, const Point & S_2){
-    vector<Point> res; // vecteur de points qui contient nos deux coordonnées 
-    Point S_milieu = (S_1 + S_2) / 2;
-    Point debut = S_milieu; // la 1ère coordonnée est le milieu de nos extrémités (normales centrées)
-    res.push_back(debut);
-    Point direction = Coor_Vecteur_Normale(S_1, S_2); // la direction est donnée par la matrice de rotation
-    Point fin(S_milieu.x + direction.x, S_milieu.y + direction.y);
-    res.push_back(fin);
-    return res;
-}
-
-
-bool transfo(const Point & a, const Point & b, const Point & c){
-    vector<Point> P = Coor_Sommets_Normale(a,b);
-    vector<Point> Q = Coor_Sommets_Normale(b,c);
-    Point E1 = P[0] | P[1];
-    Point E2 = Q[0] | Q[1];
-    double angle = E1.x * E2.y - E2.x * E1.y;
-    if (angle > 0) return false; // transfo false => segment ab et bc convexe
-    return true; // transfo false => segment ab et bc concave
-}
-
-
-
-bool isOutside(Point x, Obstacle ob){
-    Point y(x.x, INT_MAX);
-    int cpt = 0;
-    list<Segment>::iterator its = ob.segValides_contour.begin();
-    for(; its!= ob.segValides_contour.end(); its++){
-        if(intersect(its->a, its->b, x, y)){
-            cpt++;
-        }
+/* Fonction print pour faire des tests
+void printSegValides(list<Segment> liste){
+    list<Segment>::iterator its = liste.begin();
+    for(; its!= liste.end(); its++){
+        cout<<"("<<its->a<<","<<its->b<<")  ";
     }
-    if(cpt%2 == 0) return true;
-    else{
-        //Could Be Better
-        //on refait une deuxieme fois le test avec une demi-droite horizontal pour gérer d'éventuel erreur quand x se trouve sur 
-        // un coté de l'obstacle
-        Point y(INT_MAX, x.y);
-        cpt = 0;
-        list<Segment>::iterator its = ob.segValides_contour.begin();
-        for(; its!= ob.segValides_contour.end(); its++){
-            if(intersect(its->a, its->b, x, y)){
-                cpt++;
-            }
-        }
-        if(cpt%2 == 0) return true;
-    }
-   return false;
+    cout<<endl;
 }
+*/
 
-
-//////////////////////////////////////////|--------------------------|///////////////////////////////////////////
-//////////////////////////////////////////| Intersection de 2 Points |///////////////////////////////////////////
-//////////////////////////////////////////|--------------------------|///////////////////////////////////////////
-
-bool ccw(const Point &A, const Point &B, const Point &C){ 
-    return ((C.y-A.y) * (B.x-A.x)) > ((B.y-A.y) * (C.x-A.x));
-}
-
-bool intersect(const Point &A, const Point &B, const Point &C, const Point &D){
-    if(A==C || A==D || B==C || B==D) return false;
-    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D);
-}
-
-bool intersect(const Segment A, const Segment B){ return intersect(A.a, A.b, B.a, B.b);}
 
 
 
