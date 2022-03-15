c5160eb2639eb84b91ad74e7e0325d447131d23b
BalKanZ <98384730+BalKanZ@users.noreply.github.com>
2022-03-15 10:08:11 +0100

    Update Obstacle.hpp

--- Obstacle.hpp
+++ Obstacle.hpp
@@ -2,6 +2,16 @@
 #define Obstacle_H
 #include "Arc.hpp" 
 
+// Avec la classe obstacle on cherche à retourner tous les segments valides d'un obstacle.
+// Un segment est dit valide si il joint deux sommets d'un obstacle sans passer par l'intérieur de cet obstacle.
+// Rmq: On a supposé dans toute cette section (et dans tout le projet) que les sommets d'un obstacle seront construits
+// dans le sens trigonométrique.
+
+////////////////////////////////// Fonction principale d'obstacle: constructionSeg() //////////////////////////////////////////
+// Cette fonction remplit deux listes: 
+//      - segValides_contour qui contient les segments qui sont sur les bords de l'obstacle
+//      - segValides_reste qui contient les segments qui joignent deux sommets de l'obstacle 
+//        et qui ne forment pas un segment sur le bord de l'obstacle
 
 
 class Obstacle
@@ -19,14 +29,39 @@ public:
 };
 
 
-// Calcul des coordonnées du vecteur normale (1 point)
+
+// On regarde d'abord si l'obstacle est convexe ou concave.
+// Ce sera très utile pour construire la liste des segmenst valides
+
+//////////////////////////////////////////|-----------------------------------|//////////////////////////////////
+//////////////////////////////////////////| Concavité/Convexité de l'obstacle |//////////////////////////////////
+//////////////////////////////////////////|-----------------------------------|//////////////////////////////////
+
+
+/**
+ * @brief Calcul des coordonnées du vecteur normale (1 point)
+ * @return les coordonnées (sous forme de point) du vecteur normale sortant au segment [S_1,S_2]
+ */
 Point Coor_Vecteur_Normale(const Point & S_1, const Point & S_2);
 
-// Calcul des coordonnées (2 points) des deux extrémités de la normale d'un segment
+/**
+ * @brief Calcul des coordonnées (2 points) des deux extrémités de la normale d'un segment
+ * @return deux points: le point de départ et celui d'arivée de la normale au segment [S_1,S_2]
+ */
 vector<Point> Coor_Sommets_Normale(const Point & S_1, const Point & S_2);
 
-// Renvoie l'angle algébrique entre les segments (a,b) et (b,c)
+/**
+ * @brief Calcul de l'angle algébrique entre les segments (a,b) et (b,c)
+ * @return true si l'angle est >= 0, false sinon
+ */
 bool transfo(const Point & a, const Point & b, const Point & c);
+
+
+//////////////////////////////|-------------------------------------------------|////////////////////////////////
+//////////////////////////////| Construction des segments valides de l'obstacle |////////////////////////////////
+//////////////////////////////|-------------------------------------------------|////////////////////////////////
+
+// Un premier outil utile est de savoir si il y a une intersection entre deux segments
 
 //////////////////////////////// Intersection de 2 segments  ////////////////////////////////// 
 bool ccw(const Point & A,const Point &B,const Point &C);
@@ -38,9 +73,13 @@ bool ccw(const Point & A,const Point &B,const Point &C);
 bool intersect(const Point &A, const Point &B, const Point &C, const Point &D); 
 
 /**
- * @return renvoie true si A intersecte B
+ * @return true si A intersecte B
  */
 bool intersect(const Segment A, const Segment B);
+
+// Un second outil utile est de savoir si un point est à l'extérieur d'un obstacle
+
+//////////////////////////////////////////| Point extérieur à l'obstacle |///////////////////////////////////////////
 
 /**
  * @brief Vérifie si un point se trouve dans un obstacle
@@ -49,12 +88,16 @@ bool intersect(const Segment A, const Segment B);
  * @return true si le point est à l'extérieur ou sur un coté de l'obstacle
  * @return false si le point x se trouve dans l'obstacle.
  */
+// Le principe de cette fonction: on prend notre point x, on trace un demi-droite vertical vers l'infini.
+// Si cette demi droite à un nombre d'intersection paire avec les contours de notre obstacle, alors x
+// est forcément à l'extérieur de l'obstacle.
 bool isOutside(Point x, Obstacle ob);
 
 /**
  * @brief Construit un obstacle et tout ses segments 
  * Construit également les segments valides entre les points non consécutifs (à la différence du constructeur Obstacle) 
  * @param V une liste de Point 
+ * @return instancier les obstacles dans le main
  */
 Obstacle ConstructObstacle(vector<Point> V);
 
